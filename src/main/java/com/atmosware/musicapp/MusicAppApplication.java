61e5451950a77a888efccb5d4f811ab50ce25866
Özge Gül <gulumozge23@gmail.com>
2023-07-30 13:41:25 +0300

    Inital commit

---
+++ src/main/java/com/atmosware/musicapp/MusicAppApplication.java
@@ -0,0 +1,13 @@
+package com.atmosware.musicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atmosware/musicapp/MusicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atmosware.musicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
